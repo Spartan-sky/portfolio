7bbfd7c8022fa18a7ef835f0743433de4fbedac6
Skylar Werner <skylar_werner@yahoo.com>
2024-10-08 09:53:00 +0200

    rotating cubes and will test GitHubPages

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,14 @@ import Cube from "./components/basic_shapes/cube.tsx";
 function App() {
   return (
     <Canvas>
-      <directionalLight position={[0, 0, 2]} />
-      <Cube pos={[0, 0, 0]} size={[1, 1, 1]} color="white" />
+      <directionalLight position={[0, 0, 1]} />
+      <ambientLight color="red" intensity={0.2} />
+      <group position={[0, 0, 0]}>
+        <Cube pos={[2, 2, 0]} size={[1, 1, 1]} color="blue" />
+        <Cube pos={[-2, 2, 0]} size={[1, 1, 1]} color="green" />
+        <Cube pos={[2, -2, 0]} size={[1, 1, 1]} color="orange" />
+        <Cube pos={[-2, -2, 0]} size={[1, 1, 1]} color="purple" />
+      </group>
     </Canvas>
   );
 }
--- src/components/basic_shapes/cube.tsx
+++ src/components/basic_shapes/cube.tsx
@@ -1,12 +1,21 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { BoxGeometry, Mesh } from "three";
+
 export default function Cube(props: {
   pos: [number, number, number];
   size: [number, number, number];
   color: string;
 }) {
   const { pos, size, color } = props;
+  const ref = useRef<Mesh>(new Mesh());
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta;
+  });
 
   return (
-    <mesh position={pos}>
+    <mesh position={pos} ref={ref}>
       <boxGeometry args={size}></boxGeometry>
       <meshStandardMaterial color={color} />
     </mesh>
