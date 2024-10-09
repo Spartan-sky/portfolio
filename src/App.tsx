be5969e9590ba0bca32d8b09db82aed4e8ccf15e
Skylar Werner <skylar_werner@yahoo.com>
2024-10-09 07:06:58 +0200

    get a basic shader running

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import { Canvas } from "@react-three/fiber";
-import Scene from "./components/scene";
+import { OrbitControls, shaderMaterial } from "@react-three/drei";
+import { Canvas, extend } from "@react-three/fiber";
+// import Scene from "./components/scene";
+// import Blob from "./components/cmp_shapes/Blob";
+import { useEffect, useRef, useState } from "react";
+import vertexShader from "./shaders/vertexShader.glsl?raw";
+import fragmentShader from "./shaders/fragmentShader.glsl?raw";
+
+const MyShaderMaterial = shaderMaterial({}, vertexShader, fragmentShader);
+
+extend({ MyShaderMaterial });
+
+const Flag = () => {
+  return (
+    <mesh
+      // ref={mesh}
+      position={[0, 0, 0]}
+      rotation={[-Math.PI / 2, 0, 0]}
+      scale={1.5}
+    >
+      <planeGeometry args={[1, 1, 32, 32]} />
+      <shaderMaterial
+        fragmentShader={fragmentShader}
+        vertexShader={vertexShader}
+        wireframe
+      />
+    </mesh>
+  );
+};
 
 function App() {
   return (
-    <Canvas>
-      <Scene />
+    <Canvas camera={{ position: [1.0, 1.0, 1.0] }}>
+      <Flag />
+      <axesHelper />
+      <OrbitControls />
     </Canvas>
   );
 }
---
+++ src/components/cmp_shapes/Blob.tsx
@@ -0,0 +1,5 @@
+import Sphere from "../basic_shapes/sphere";
+
+export default function Blob() {
+  return <Sphere />;
+}
